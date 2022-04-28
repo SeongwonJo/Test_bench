b3e08edcff9f5e1a211a728560e05ddae3a93c5f
SeongwonJo <97574159+SeongwonJo@users.noreply.github.com>
2022-04-28 17:50:44 +0900

    Update alarm.py

--- utils/alarm.py
+++ utils/alarm.py
@@ -2,7 +2,7 @@ import requests
 
 
 def send_alarm_to_slack(msg):
-    url = "https://hooks.slack.com/services/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"
+    url = "slack web hook"
 
     payload = {"text" : msg }
 
