07e126f677a168eb5fb1b2f6f184d3ba83355af9
SeongwonJo <anjelrin94@gmail.com>
2022-05-25 12:01:10 +0900

    make simple

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ from trainer import Training, evaluate
 import inference
 
 
+def argument():
+    parser = argparse.ArgumentParser(description='PyTorch Image Classification')
+    parser.add_argument("--yml_path", required=True, default="./properties.yml",
+                        help="path to yml file contains experiment options")
+    parser.add_argument("--device", default="cuda:0")
+    parser.add_argument("--csv_path", default='/home/work/test1/result.csv')
+    parser.add_argument("--task", default="train")
+    arguments = parser.parse_args()
+    return arguments
+
+
 def sequential_task(tasklist, device, csv_path):
     for i in tasklist:
         trainer = Training(tasklist[i], index_num=i, device=device, csv_path=csv_path)
@@ -15,20 +26,24 @@ def sequential_task(tasklist, device, csv_path):
     send_alarm_to_slack("All task done")
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='PyTorch Image Classification')
-    parser.add_argument("--yml_path", required=True, default="./properties.yml",
-                        help="path to yml file contains experiment options")
-    parser.add_argument("--device", default="cuda:0")
-    parser.add_argument("--csv_path", default='/home/work/test1/result.csv')
-    parser.add_argument("--task", default="train")
-    args = parser.parse_args()
-    if args.task == 'train':
+def inference_task(yml_path):
+    o_dict = inference.yml_to_dict(yml_path)
+    test_loader, model, device = inference.initialization(o_dict)
+    test_accuracy = evaluate(model, test_loader, device)
+    print('Test Accuracy: {:.2f}%'.format(test_accuracy))
+
+
+def select_task(task):
+    if task == 'train':
         tasklist = yml_to_tasklist(args.yml_path)
         sequential_task(tasklist, args.device, args.csv_path)
-    elif args.task == 'inference':
-        o_dict = inference.yml_to_dict(args.yml_path)
-        test_loader, model, device = inference.initialization(o_dict)
-        test_accuracy = evaluate(model, test_loader, device)
-        print('Test Accuracy: {:.2f}%'.format(test_accuracy))
+    elif task == 'inference':
+        inference_task(args.yml_path)
+
+
+if __name__ == '__main__':
+    args = argument()
+    select_task(args.task)
+
+
 
