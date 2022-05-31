9bd7d6683ee6a80df06901ac0daecf6beef7db1c
SeongwonJo <anjelrin94@gmail.com>
2022-05-31 13:34:23 +0900

    under construction

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import argparse
 
 from utils.alarm import send_alarm_to_slack
 from utils.yml_to_tasklist import yml_to_tasklist
-from trainer import Training, evaluate
-import inference
+from trainer import Training
 
 
 def argument():
@@ -27,23 +26,20 @@ def sequential_task(tasklist, device, csv_path):
 
 
 def inference_task(yml_path):
-    o_dict = inference.yml_to_dict(yml_path)
-    test_loader, model, device = inference.initialization(o_dict)
-    test_accuracy = evaluate(model, test_loader, device)
-    print('Test Accuracy: {:.2f}%'.format(test_accuracy))
+    print('will be implemented')
 
 
-def select_task(task):
-    if task == 'train':
-        tasklist = yml_to_tasklist(args.yml_path)
-        sequential_task(tasklist, args.device, args.csv_path)
-    elif task == 'inference':
-        inference_task(args.yml_path)
+def select_task(arguments):
+    if arguments.task == 'train':
+        tasklist = yml_to_tasklist(arguments.yml_path)
+        sequential_task(tasklist, arguments.device, arguments.csv_path)
+    elif arguments.task == 'inference':
+        inference_task(arguments.yml_path)
 
 
 if __name__ == '__main__':
     args = argument()
-    select_task(args.task)
+    select_task(args)
 
 
 
